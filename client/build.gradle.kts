c7658938eea12f900b604f96ef7a22a340a43f27
70ekanetugu <12kanetugu@gmail.com>
2023-01-28 23:53:53 +0900

    add publishing to build.gradle.kts

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     kotlin("jvm") version "1.8.0"
     application
+    id("maven-publish")
 }
+
+
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -14,4 +18,22 @@ dependencies {
 
 application {
     mainClass.set("com.example.client.HelloClientKt")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "github-package-registry"
+            url = uri("https://maven.pkg.github.com/70ekanetugu/grpc-with-kotlin")
+            credentials {
+                username = project.findProperty("GITHUB_USER") as? String ?: ""
+                password = project.findProperty("GITHUB_TOKEN") as? String ?: ""
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
